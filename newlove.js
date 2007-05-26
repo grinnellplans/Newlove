eb2362180ad98a2dabda5503c40de5192a0daafc
Ian Young <youngian@grinnell.edu>
2007-05-26 16:29:10 -0500

    Added revision history and copyright date

--- newlove.js
+++ newlove.js
@@ -1,4 +1,6 @@
 /*
+(c) Ian Young 2007
+
 This program is free software; you can redistribute it and/or modify it 
 under the terms of the GNU General Public License as published by the Free 
 Software Foundation; either version 2 of the License, or (at your option) 
@@ -13,6 +15,9 @@ You should have received a copy of the GNU General Public License along
 with this program; if not, write to the Free Software Foundation, Inc., 59 
 Temple Place, Suite 330, Boston, MA 02111-1307 USA 
 */
+
+// Revision History
+// 1.0 - Initial release.
 
 // ==UserScript==
 // @name           NewLove
